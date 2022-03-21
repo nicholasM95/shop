70050b3bedd96b6a62522cd2899ea028f0df07ed
Nicholas Meyers <nicholas@sinners.be>
2022-03-22 00:20:10 +0100

    fix NPE

--- src/main/java/be/nicholas/shop/core/filter/TelemetryUserFilter.java
+++ src/main/java/be/nicholas/shop/core/filter/TelemetryUserFilter.java
@@ -1,6 +1,7 @@
 package be.nicholas.shop.core.filter;
 
 import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
+import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
 import com.microsoft.applicationinsights.web.internal.ThreadContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,11 +17,16 @@ public class TelemetryUserFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        RequestTelemetryContext threadContext = ThreadContext.getRequestTelemetryContext();
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
 
-        RequestTelemetry requestTelemetry = ThreadContext.getRequestTelemetryContext().getHttpRequestTelemetry();
-        requestTelemetry.getContext().getUser().setId(username);
+        if (threadContext != null) {
+            RequestTelemetry requestTelemetry = threadContext.getHttpRequestTelemetry();
+            requestTelemetry.getContext().getUser().setId(username);
+        }
+
         chain.doFilter(request, response);
     }
 }
